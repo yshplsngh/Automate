510079b417e8d3f5622b9a114cccc13800b39d65
Yashpal Singh <yashpalsingh9rx@outlook.com>
2024-12-27 07:54:42 +0530

    added job creation route

---
+++ backend/src/routes/job.ts
@@ -0,0 +1,35 @@
+import { Router, Request, Response } from "express";
+import db from "../db"
+import { Job } from "../types";
+
+const jobRouter = Router();
+
+jobRouter.post("/create", async (req: Request, res: Response): Promise<void> => {
+    const jobData = req.body as Job;
+    try {
+        const job = await db.job.create({
+            data: {
+                name: jobData.name,
+                description: jobData.description,
+                type: jobData.type,
+                workflow_id: jobData.workflowId,
+                app: jobData.app,
+                step_no: jobData.step,
+                data: jobData.data,
+            }
+        })
+        res.status(200).json({
+            success: true,
+            message: "Job successfully created.",
+            data: job
+        })
+        return;
+    }catch(err: any){
+        console.log("Error api/job/create;", err);
+        res.status(500).json({
+            success: false,
+            message: "An unexpected error has happened.",
+        })
+        return;
+    }
+})
