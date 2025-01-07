930302c4bfb7083b37f508c67c489c1a85d9ce24
Yashpal Singh <yashpalsingh9rx@outlook.com>
2025-01-07 22:34:19 +0530

    added scheduler

--- workers/executor/apps/http/schema.go
+++ workers/executor/apps/http/schema.go
@@ -7,15 +7,15 @@ type HttpJob struct {
 }
 
 type HttpJobInput struct {
-	URL        string            `json:"url"`        // Must be a valid URL
-	Method     string            `json:"method"`     // Enum of HTTP methods
-	Headers    map[string]string `json:"headers"`    // Record of header key-value pairs
-	Parameters map[string]string `json:"parameters"` // Query parameters as key-value pairs
-	Body       string            `json:"body"`       // Request body
+	URL        string            `json:"url"`
+	Method     string            `json:"method"`
+	Headers    map[string]string `json:"headers"`
+	Parameters map[string]string `json:"parameters"`
+	Body       string            `json:"body"` // stringified Request body
 }
 
 type HttpJobOutput struct {
-	StatusCode int                 `json:"statusCode"` // HTTP status code
-	Headers    map[string][]string `json:"headers"`    // Response headers
-	Body       string              `json:"body"`       // Response body
+	StatusCode int                 `json:"statusCode"`
+	Headers    map[string][]string `json:"headers"`
+	Body       string              `json:"body"`
 }
